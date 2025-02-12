26e7817d49692331bc2d5d01061a157d8d0bd5c6
clemence avrillon <clemsam2409@gmail.com>
2025-02-12 10:55:50 +0000

    structure du projet

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,12 +1,15 @@
-select
--- Operational_margin = margin + shipping_fee - log_cost - ship_cost
-date_date,
-orders_id,
-cast((margin + shipping_fee - logcost - ship_cost) as numeric) as opereational_margin
-
-from
- {{ref('int_orders_margin')}} 
-join
-{{ref("stg_raw__ship")}} 
-using (orders_id)
-order by orders_id desc
+SELECT
+     o.orders_id
+     ,o.date_date
+     ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin
+     ,o.quantity
+     ,o.revenue
+     ,o.purchase_cost
+     ,o.margin
+     ,s.shipping_fee
+     ,s.logcost
+     ,s.ship_cost
+ FROM {{ref("int_orders_margin")}} o
+ LEFT JOIN {{ref("stg_raw__ship")}} s
+     USING(orders_id)
+ ORDER BY orders_id desc
